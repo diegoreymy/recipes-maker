0a0edc9ea297e33d5a8c2aabc8adf0b27d7945cc
Diego Reymy <diegoreymy@mgmail.com>
2019-12-06 17:44:31 -0300

    Reto-1 v2

--- src/menu.kt
+++ src/menu.kt
@@ -1,8 +1,4 @@
 fun main() {
-    menu()
-}
-
-fun menu() {
     val options = listOf<String>("Hacer una receta", "Ver mis recetas", "Salir")
     val title = ":: Bienvenido a Recipe Maker ::\n"
     val subTitle = "Selecciona la opción deseada"
@@ -13,16 +9,16 @@ fun menu() {
     for ((index, option) in options.withIndex()) {
         println("${index.plus(1)} - $option")
     }
-
+    
     do {
         val userResponse: String? = readLine()
         try {
             when (val response: Int = userResponse?.toInt() ?: 0) {
                 in 1..3 -> println(options[response.minus(1)])
-                else -> println("Opción Inválida")
+                else -> println("Opción Inválida: No existe una opción con ese número")
             }
         } catch (e: NumberFormatException) {
-            println("Opción Inválida")
+            println("Opción Inválida: Eso no es un número, por favor ingrese un número válido")
         }
     }while (!(userResponse?.equals("3"))!!)
 }
